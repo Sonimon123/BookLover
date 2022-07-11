ab5d0479bd0e65630f799a90cc87a95d9bb93411
Sonimon123 <jrkargbo@aol.com>
2022-07-11 16:15:39 -0400

    First Commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='BookLover',
+      version='0.1',
+      description="Stores information about people who love books, the books they've read, and how much they like them.",
+      url='https://github.com/Sonimon123/BookLover',
+      author='JR Kargbo',
+      license='MIT',
+      packages=['booklover'])
